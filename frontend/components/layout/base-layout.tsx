9284da74632c0d3bed1d0c6ac0470768e10ab051
fachryikhsal <fachryikhsal890@gmail.com>
2026-05-05 19:58:15 +0700

    fix(layout): container is not centered on a bigger screen add: custom favicon

--- frontend/components/layout/base-layout.tsx
+++ frontend/components/layout/base-layout.tsx
@@ -15,7 +15,7 @@ const BaseLayout = ({
   return (
     <>
       <ScrollProgress />
-      <div className="container min-h-screen pt-12 sm:pt-24 px-6">
+      <div className="container mx-auto min-h-screen pt-12 sm:pt-24 px-6">
         <div className="fixed inset-0 flex items-center justify-center overflow-hidden h-40 pointer-events-none z-[-1]">
           <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden bg-background [-webkit-mask-image:linear-gradient(to_bottom,black,transparent)]">
             <FlickeringGrid
